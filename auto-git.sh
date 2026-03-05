ae6794221f51cb66832343c45196935acbe08ea9
Marvyn Souza Madeira <marvynmadeira@gmail.com>
2026-03-05 18:00:07 -0300

    Add menu options

--- auto-git.sh
+++ auto-git.sh
@@ -55,5 +55,20 @@ selected=$(echo $selected | tr -d '* ')
 git branch -d "$selected"
 }
 
-delete_branch
+function main (){
+    options=(\
+    "1 - Switch Branch" \
+    "2 - Git Merge" \
+    "3 - Delete Branch" \
+    "Exit" \
+    )
 
+    selected=$(for opt in "${options[@]}" ; do echo $opt ; done | fzf +m \
+    --height 40% \
+    --layout reverse \
+    --border \
+    --color bg:#222222 \
+    )
+}
+
+main
