4e19ded1251befc5d0e88329d5ba6b7ba43c2c88
Marvyn Souza Madeira <marvynmadeira@gmail.com>
2026-03-05 16:27:53 -0300

    Add function switch branch

--- auto-git.sh
+++ auto-git.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-selected=$(git branch | fzf \
+function switch_branch () {
+
+    selected=$(git branch | fzf +m \
 --height 40% \
 --layout reverse \
 --border \
---preview 'git log --oneline')
+--preview 'git log --oneline $(echo {} | tr -d "* ")' \
+--color bg:#222222,preview-bg:#333333)
+
+if [ $? -eq 130 ]; then
+    echo "Exiting..."
+    exit 1
+
+fi
 
 selected=$(echo $selected | tr -d '* ')
 
 echo "$selected"
 
 git switch "$selected"
+}
+
