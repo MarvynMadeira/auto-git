542b78fd5a331c30838a3723cb890aed80507b41
Marvyn Souza Madeira <marvynmadeira@gmail.com>
2026-03-07 13:31:08 -0300

    operator-git.sh has been cleaned for work

--- operator-git.sh
+++ operator-git.sh
@@ -1,7 +1,1 @@
 #!/bin/bash 
-
-#!/bin/bash
-echo "------------------------------------------"
-echo "   BEM-VINDO AO PAINEL DO OPERADOR"
-echo "------------------------------------------"
-# Coloque aqui as opções de Merge, Release, etc.
