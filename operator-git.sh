8cb27c75bb50d6bcf290dac12556562d8b76974a
Marvyn Souza Madeira <marvynmadeira@gmail.com>
2026-03-07 13:26:15 -0300

    Add feature/new_painel branch in develop

---
+++ operator-git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+#!/bin/bash
+echo "------------------------------------------"
+echo "   BEM-VINDO AO PAINEL DO OPERADOR"
+echo "------------------------------------------"
+# Coloque aqui as opções de Merge, Release, etc.
